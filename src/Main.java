5da2c882d69b47aed53e569ef9e59069d02ad410
ACER\dmitr <dmitry.grishanov@gmail.com>
2023-02-11 11:04:55 -0600

    updated project

--- src/Main.java
+++ src/Main.java
@@ -2,80 +2,92 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int resultCalc = 0;
-        int tradesTo25K =0;
 
 
-        float percentPerTradeFromStartAmount = 2.0F;
-        int startAmount = 500;
-        int weeks = 52;
-        int tradesPerWeek =10;
-        int tradesToEven =0;
-        int currentWeek = 26;
+        int startAmount = 2000;
+
+        int currentWeek = 1;
+
+        float percentPerTradeFromStartAmount =     2.F;
+        float percentAfterReducing =               1.5F;
+        int   whenToReduce =                       12000;
+
+
+
+        int weeks = 52 ;
+        int tradesPerWeek =  5;
+
+
+
+
         // ToDo add limit for max bid
-        int PLPercent = 2;
-        int whenToReduce = 25000;
-        float percentAfterReducing = 1.0F;
+        int PLPercentPerTrade = 5;
+
+
         // ToDo create method to overwrite number of trades
         int tradesPerWeekAfter25K =10;
 
-        resultCalc = calcProcDay(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek, PLPercent, percentAfterReducing,currentWeek,whenToReduce);
-        tradesTo25K = tradesTo25K(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek, percentAfterReducing, whenToReduce);
-        tradesToEven = tradesToEven(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek);
+        int resultCalc = calcProcDay(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek, PLPercentPerTrade, percentAfterReducing,currentWeek,whenToReduce);
+        int tradesToReducedPercent = tradesToReducedPercent(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek, percentAfterReducing, whenToReduce);
+        int tradesToEven = tradesToEven(percentPerTradeFromStartAmount, startAmount, weeks, tradesPerWeek);
         //resultCalc = calcAmountDay(50, 2000, 130);
 
 
-        System.out.println("Total= "+resultCalc/1000+"k for "+ weeks+" weeks");
-        System.out.println("Trades to even= "+tradesToEven);
+        System.out.println("Start amount = "+startAmount);
+        System.out.println("Total Account = "+resultCalc/1000+"k in "+ weeks+" weeks");
+        System.out.println("Trades to even (you get your $"+startAmount+" back) = "+tradesToEven);
 
-        System.out.println("Weeks to even= "+tradesToEven/tradesPerWeek);
-        System.out.println("Trades to 25k= "+tradesTo25K);
-        System.out.println("Weeks to 25k= "+tradesTo25K/tradesPerWeek);
+        System.out.println("Weeks to even (you get your $"+startAmount+" back) = "+tradesToEven/tradesPerWeek);
+        System.out.println("Trades to reduced Percent (reduce after you made $"+whenToReduce+" ) = " +tradesToReducedPercent);
+        System.out.println("Weeks  to reduced Percent (reduce after you made $"+whenToReduce+" ) = "+tradesToReducedPercent/tradesPerWeek);
 
     }
 
 
 
     public static int calcProcDay(Float percentPerTradeFromStartAmount, Integer start, Integer weeks, int tradesPerWeek, int minimumBidPercentOfTotal, float percentAfter25K, int currentWeek, int whenToReduce){
-      int result =start;
-      int PL = 0 ;
-      int trades= weeks*tradesPerWeek;
-      int week =0;
-      int minimumBid = 0 ;
+        int result =start;
+        int PL = 0 ;
+        int trades= weeks*tradesPerWeek;
+        int week =0;
+        int minimumBid = 0 ;
 
 
-       for (int i=1; i<=trades; i++) {
-           PL = (int) (percentPerTradeFromStartAmount * result / 100);
-           result = (int) (percentPerTradeFromStartAmount * result / 100 + result);
-           week = (i)/tradesPerWeek+1;
-           minimumBid = PL * 100/minimumBidPercentOfTotal;
+        for (int i=1; i<=trades; i++) {
+            PL = (int) (percentPerTradeFromStartAmount * result / 100);
+            result = (int) (percentPerTradeFromStartAmount * result / 100 + result);
+            week = (i)/tradesPerWeek+1;
+            minimumBid = PL * 100/minimumBidPercentOfTotal;
 
-           if (result>whenToReduce) {
-               percentPerTradeFromStartAmount = percentAfter25K;
-           }
-
-
-
-           if (currentWeek==week)
-           {
-               System.out.println(
-                       "  ||  Week #= "+week+
-                       "  ||  Trade # = "+i+
-                       //"  percent per trade = "+percentPerTradeFromStartAmount+
-
-                       "  ||  Trade# "+i+
-                       "  ||  Minimum P/L per Trade = "+ PL+
-                       "  ||  Total= "+result +
-                       "  ||  minimum bid ("+minimumBidPercentOfTotal+" % of grow for P/L )= " + minimumBid);
-           }
+            if (result>whenToReduce) {
+                percentPerTradeFromStartAmount = percentAfter25K;
+            }
 
 
 
+            if
+                //(
+            (currentWeek==week)
+            //||((currentWeek+1)==week))
+            {
+                System.out.println(
+                        "  ||  Week # = "+week+
+                                "  ||  Trade # = "+i+
+                                "  ||  percent per trade from start Amount = "+percentPerTradeFromStartAmount+" %"+
 
-       }
+                                //"  ||  Trade# "+i+
+                                "  ||  Minimum P/L per Trade = "+ PL+
+                                "  ||  Total = "+result +
+                                "  ||  minimum bid ("+minimumBidPercentOfTotal+" % of grow for P/L )= " + minimumBid);
+            }
+
+
+
+
+        }
         return result;
     }
-    public static int tradesTo25K(Float proc, Integer start, Integer weeks, int tradesPerWeek, float percentAfterReducing, int whenToReduce){
+    public static int tradesToReducedPercent(Float proc, Integer start, Integer weeks, int tradesPerWeek, float percentAfterReducing, int whenToReduce){
         int result =start;
         int daily = 0 ;
         int trades= weeks*tradesPerWeek;
@@ -86,7 +98,7 @@ public class Main {
             result = (int) (proc * result / 100 + result);
             if (result>whenToReduce){whenToReducePercent = i;
 
-            week = (i-1)/tradesPerWeek;
+                week = (i-1)/tradesPerWeek;
 
                 return whenToReducePercent;
             }
